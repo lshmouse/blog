6b68a9e1e5a237af667107bc99ed75e9b22378db
lshmouse@gmail.com <lshmouse@gmail.com>
2013-03-10 00:52:30 +0800

    a small change

---
+++ programmining-contests/topcoder/TCO2013/round1A/HouseBuilding.java
@@ -0,0 +1,204 @@
+// BEGIN CUT HERE
+
+// END CUT HERE
+import java.util.*;
+
+public class HouseBuilding {
+	public int getMinimum(String[] area) {
+		int res = Integer.MAX_VALUE;
+		for (int i = 0; i < 10; i++) {
+			int tmp = 0;
+			for (int x = 0; x < area.length; x++)
+				for (int y = 0; y < area[x].length(); y++) {
+					int h = (area[x].charAt(y) - '0');
+					if (h > i)
+						tmp += h - 1 - i;
+					else
+						tmp += i - h;
+				}
+			res = Math.min(res, tmp);
+		}
+		return res;
+	}
+
+	// BEGIN CUT HERE
+	public static void main(String[] args) {
+		try {
+			eq(0,
+					(new HouseBuilding())
+							.getMinimum(new String[] { "10", "31" }), 2);
+			eq(1,
+					(new HouseBuilding()).getMinimum(new String[] { "54454",
+							"61551" }), 7);
+			eq(2, (new HouseBuilding()).getMinimum(new String[] { "989" }), 0);
+			eq(3, (new HouseBuilding()).getMinimum(new String[] { "90" }), 8);
+			eq(4,
+					(new HouseBuilding()).getMinimum(new String[] { "5781252",
+							"2471255", "0000291", "1212489" }), 53);
+		} catch (Exception exx) {
+			System.err.println(exx);
+			exx.printStackTrace(System.err);
+		}
+	}
+
+	private static void eq(int n, int a, int b) {
+		if (a == b)
+			System.err.println("Case " + n + " passed.");
+		else
+			System.err.println("Case " + n + " failed: expected " + b
+					+ ", received " + a + ".");
+	}
+
+	private static void eq(int n, double a, double b) {
+		if (Math.abs(a - b) < 1e-6)
+			System.err.println("Case " + n + " passed.");
+		else
+			System.err.println("Case " + n + " failed: expected " + b
+					+ ", received " + a + ".");
+	}
+
+	private static void eq(int n, char a, char b) {
+		if (a == b)
+			System.err.println("Case " + n + " passed.");
+		else
+			System.err.println("Case " + n + " failed: expected '" + b
+					+ "', received '" + a + "'.");
+	}
+
+	private static void eq(int n, long a, long b) {
+		if (a == b)
+			System.err.println("Case " + n + " passed.");
+		else
+			System.err.println("Case " + n + " failed: expected \"" + b
+					+ "L, received " + a + "L.");
+	}
+
+	private static void eq(int n, boolean a, boolean b) {
+		if (a == b)
+			System.err.println("Case " + n + " passed.");
+		else
+			System.err.println("Case " + n + " failed: expected " + b
+					+ ", received " + a + ".");
+	}
+
+	private static void eq(int n, String a, String b) {
+		if (a != null && a.equals(b))
+			System.err.println("Case " + n + " passed.");
+		else
+			System.err.println("Case " + n + " failed: expected \"" + b
+					+ "\", received \"" + a + "\".");
+	}
+
+	private static void eq(int n, int[] a, int[] b) {
+		if (a.length != b.length) {
+			System.err.println("Case " + n + " failed: returned " + a.length
+					+ " elements; expected " + b.length + " elements.");
+			return;
+		}
+		for (int i = 0; i < a.length; i++)
+			if (a[i] != b[i]) {
+				System.err
+						.println("Case "
+								+ n
+								+ " failed. Expected and returned array differ in position "
+								+ i);
+				print(b);
+				print(a);
+				return;
+			}
+		System.err.println("Case " + n + " passed.");
+	}
+
+	private static void eq(int n, long[] a, long[] b) {
+		if (a.length != b.length) {
+			System.err.println("Case " + n + " failed: returned " + a.length
+					+ " elements; expected " + b.length + " elements.");
+			return;
+		}
+		for (int i = 0; i < a.length; i++)
+			if (a[i] != b[i]) {
+				System.err
+						.println("Case "
+								+ n
+								+ " failed. Expected and returned array differ in position "
+								+ i);
+				print(b);
+				print(a);
+				return;
+			}
+		System.err.println("Case " + n + " passed.");
+	}
+
+	private static void eq(int n, String[] a, String[] b) {
+		if (a.length != b.length) {
+			System.err.println("Case " + n + " failed: returned " + a.length
+					+ " elements; expected " + b.length + " elements.");
+			return;
+		}
+		for (int i = 0; i < a.length; i++)
+			if (!a[i].equals(b[i])) {
+				System.err
+						.println("Case "
+								+ n
+								+ " failed. Expected and returned array differ in position "
+								+ i);
+				print(b);
+				print(a);
+				return;
+			}
+		System.err.println("Case " + n + " passed.");
+	}
+
+	private static void print(int a) {
+		System.err.print(a + " ");
+	}
+
+	private static void print(long a) {
+		System.err.print(a + "L ");
+	}
+
+	private static void print(String s) {
+		System.err.print("\"" + s + "\" ");
+	}
+
+	private static void print(int[] rs) {
+		if (rs == null)
+			return;
+		System.err.print('{');
+		for (int i = 0; i < rs.length; i++) {
+			System.err.print(rs[i]);
+			if (i != rs.length - 1)
+				System.err.print(", ");
+		}
+		System.err.println('}');
+	}
+
+	private static void print(long[] rs) {
+		if (rs == null)
+			return;
+		System.err.print('{');
+		for (int i = 0; i < rs.length; i++) {
+			System.err.print(rs[i]);
+			if (i != rs.length - 1)
+				System.err.print(", ");
+		}
+		System.err.println('}');
+	}
+
+	private static void print(String[] rs) {
+		if (rs == null)
+			return;
+		System.err.print('{');
+		for (int i = 0; i < rs.length; i++) {
+			System.err.print("\"" + rs[i] + "\"");
+			if (i != rs.length - 1)
+				System.err.print(", ");
+		}
+		System.err.println('}');
+	}
+
+	private static void nl() {
+		System.err.println();
+	}
+	// END CUT HERE
+}
---
+++ programmining-contests/topcoder/TCO2013/round1A/solutions.md
@@ -0,0 +1,20 @@
+TCO 2013 round1A
+=============================
+
+
+HouseBuilding
+---------------
+A very simple question. Just enumerate the low height of ground and calculate
+the cost. At last, choose the height with lowest cost.
+
+TheFrog
+-----------------
+A very intereting problem! The main point is that the length of length is
+limited by the pits and there must be a right point of pit you jumped on it.
+So just enumerate which point you must jumped on it and how many steps you
+jumped on it from zero.
+
+DirectionBoard
+---------------------
+Change it to minicost maxflow model.
+
